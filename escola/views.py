8db80b9a4e2c33925034030011108f2bf99d0c34
Anderson <andersontno@gmail.com>
2022-03-25 17:05:21 -0300

    Trablhando com permissões

--- escola/views.py
+++ escola/views.py
@@ -1,3 +1,4 @@
+from django import http
 from rest_framework import viewsets, generics
 from escola.models import Aluno, Curso, Matricula
 from escola.serializer import AlunoSerializer, AlunoSerializerV2, CursoSerializer, MatriculaSerializer, ListaMatriculasAlunoSerializer, ListaAlunosMatriculadosSerializer
@@ -22,6 +23,7 @@ class MatriculaViewSet(viewsets.ModelViewSet):
     """Listando todas as matrículas"""
     queryset = Matricula.objects.all()
     serializer_class = MatriculaSerializer
+    http_method_names = ['get', 'post', 'put', 'path']
     
 class ListaMatriculasAluno(generics.ListAPIView):
     """Listando as matrículas de um aluno ou aluna"""
